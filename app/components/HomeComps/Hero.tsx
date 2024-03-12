11ecaa75215663145645eafb16acfb9de2393622
BeanieMen <jainarjav886@gmail.com>
2024-03-12 15:57:12 +0530

    a single fucking quote

--- app/components/HomeComps/Hero.tsx
+++ app/components/HomeComps/Hero.tsx
@@ -59,7 +59,7 @@ export const Hero = () => {
                     }}
                 >
                     <h1 className='mx-5 bg-gradient-to-b from-black via-black/90 to-black/70 to-90% bg-clip-text font-title text-2xl font-bold leading-9 text-transparent dark:from-white dark:via-white/90 dark:to-white/70 sm:text-4xl sm:leading-[3.5rem]'>
-                        I'm Aarjav, A Kid creating{' '}
+                        I&apos;m Aarjav, A Kid creating{' '}
                         <div className='inline-grid h-9 overflow-hidden sm:h-[3.5rem]'>
                             <div ref={scope}>
                                 {TEXTS.map(({ text, className }, index) => (
